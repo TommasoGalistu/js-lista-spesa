c3b2e6d6d3274866f88eecc402957a53d1c3204b
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-11 14:27:49 +0200

    add: cicle for populate arr spesa

--- js/myjs.js
+++ js/myjs.js
@@ -1,5 +1,31 @@
 // chiedo all'utente la lista della spesa
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
+let i = 0;
+let arrSpesaInput = []
+let spesaDefault = [   
+    'carciofi',
+    'altri carciofi',
+    'carote',
+    'pane',
+    'uova',
+    'acqua'
+]
+
+while(i < 2 ){
+    let input = prompt("scrivi la spesa o scrivi 'spesa default'")
+    if(input !== 'spesa default'){
+        arrSpesaInput.push(input)
+        i++
+    }else{
+        break
+    }
 }
+
+console.log(arrSpesaInput)
+
+// crea la lista della spesa
+// i = 0;
+// if(input === 'spesa default'){
+//     while(i < spesaDefault.length -1){
+//         console.log(spesaDefault[i])
+//     }
+// }
