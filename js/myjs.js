c2eb228dc01e46fcc2bcb75adbbc5a90841dca17
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-11 15:29:27 +0200

    add: event for eliminate list element

--- js/myjs.js
+++ js/myjs.js
@@ -14,7 +14,7 @@ let spesaDefault = [
     'acqua'
 ]
 let inputUtente;
-while(i < 2){
+while(i < 5){
     let input = prompt("scrivi la spesa o scrivi 'spesa default'")
     if(input !== 'spesa default'){
         arrSpesaInput.push(input)
@@ -25,7 +25,7 @@ while(i < 2){
     }
 }
 
-console.log(inputUtente)
+
 
 // crea la lista della spesa
 i = 0;
@@ -67,6 +67,7 @@ if(inputUtente === 'spesa default'){
         // aggiungo classe a elemento i
         icon.classList.add('fa-solid')
         icon.classList.add('fa-x')
+        elementSpesa.classList.add('listaSpesa')
         // appendo i a li
         li_contIcon.append(icon)
         // appenso elemento lista a ul
@@ -77,3 +78,18 @@ if(inputUtente === 'spesa default'){
         console.log(elementSpesa)
     }
 }
+
+// creo delle variabili con la lista degli elementi da eliminare
+let eliminate = document.getElementsByClassName('fa-x')
+let lista = document.getElementsByClassName('listaSpesa')
+
+// creo un evento per eliminare con il click gli elementi in lista
+contIcon.addEventListener('click', (event) =>{
+    // trovo la posizione dell'elemento cliccato
+    let posizione = Array.from(eliminate).indexOf(event.target)
+    
+    // gli aggiungo la classe elimina
+    eliminate[posizione].classList.add('close')
+    lista[posizione].classList.add('close')
+})
+
